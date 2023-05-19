e37580d91fe6cdc71803f8e41a62c75a3089dcdf
Никита Стратий <nikita@Nikitas-MacBook-Pro.local>
2023-05-19 06:56:38 +0300

    feat: setup postgresql

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export const SQL_TYPE = 'postgres';
+export const SQL_HOST = process.env.SQLHOST;
+export const SQL_PORT = Number(process.env.SQLPORT);
+export const SQL_USER = process.env.SQLUSER;
+export const SQL_PASSWORD = process.env.SQLPASSWORD;
+export const SQL_DATABASE = process.env.SQLDATABASE;
